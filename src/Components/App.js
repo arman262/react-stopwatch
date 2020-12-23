25cc435c1722f7802ac80bfb0cb601aa93175604
Md Arman AL Sharif <49965488+arman262@users.noreply.github.com>
2020-12-23 17:26:01 +0600

    complete stopwatch

---
+++ src/Components/App.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import './App.css';
+import Controller from './Controller/Controller';
+import CountDown from './CountDown/CountDown';
+import Title from './Title/Title';
+
+class App extends Component {
+
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      time : {
+        min:  0,
+        sec: 0, 
+        mili: 0,
+      },
+    }
+  }
+  getStarted(){
+    this.intervalID = setInterval(() => {
+      let min = this.state.time.mim
+      let sec = this.state.time.sec
+      let mili = this.state.time.mili
+
+      if (mili >= 10) {
+        sec = sec + 1
+        mili = 0
+      }
+      if (sec >= 60){
+        min = min +1
+        sec = 0
+      }
+      
+      this.setState ({
+        ...this.state,
+        time: {
+          min,
+          sec,
+          mili: mili +1
+        }
+      })
+    }, 100);
+  }
+
+  getPause() {
+    clearInterval(this.intervalID) 
+  }
+
+
+  getLap() {
+
+  }
+
+  getReset(){
+    this.setState({
+      time : {
+        min:  0,
+        sec: 0, 
+        mili: 0,
+      },
+    })
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container py-5">
+          <div className="row">
+            <div className="col-sm-8 offset-sm-2">
+              <Title/>
+              <CountDown time={ this.state.time } />
+              <Controller 
+                start = { this.getStarted.bind(this) }
+                pause = { this.getPause.bind(this) }
+                reset = { this.getReset.bind(this)}
+              /> 
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
